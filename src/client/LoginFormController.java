422e90b848f0776c8c079a090417b96fbb148911
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-14 22:16:11 +0530

    Updated LoginFormController

--- src/client/LoginFormController.java
+++ src/client/LoginFormController.java
@@ -3,6 +3,7 @@ package client;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
@@ -11,6 +12,14 @@ public class LoginFormController {
     public TextField txtUsername;
     public PasswordField txtPassword;
     public Button btnSignUp;
+    public Pane pnSignUp;
+    public TextField txtRegFirstname;
+    public TextField txtRegname;
+    public TextField txtRegPassword;
+    public TextField txtRegEmail;
+    public TextField txtRegPhoneNo;
+    public RadioButton male;
+    public RadioButton female;
 
     public void login(ActionEvent actionEvent) {
     }
