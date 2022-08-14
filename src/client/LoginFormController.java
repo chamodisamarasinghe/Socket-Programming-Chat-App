c394c95b64880953492e0fe2b3050a4d7f5bfcb2
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-14 21:06:07 +0530

    Added LoginFormController

---
+++ src/client/LoginFormController.java
@@ -0,0 +1,20 @@
+package client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class LoginFormController {
+    public Pane pnSignIn;
+    public TextField txtUsername;
+    public PasswordField txtPassword;
+    public Button btnSignUp;
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void handleButtonAction(ActionEvent actionEvent) {
+    }
+}
