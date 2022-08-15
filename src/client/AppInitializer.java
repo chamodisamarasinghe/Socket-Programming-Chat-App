e6288d15ef543e58dd6bed4c01182189fb457085
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-15 15:50:17 +0530

    Added AppInitializer

---
+++ src/client/AppInitializer.java
@@ -0,0 +1,19 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("LoginForm.fxml"))));
+        primaryStage.show();
+    }
+}
